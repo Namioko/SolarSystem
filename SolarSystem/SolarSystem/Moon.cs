853b8973280e336f48e856d1712315fec85b4afc
thequeen14 <flamina96@yandex.ru>
2016-09-28 21:59:53 +0300

    Started with classes

---
+++ SolarSystem/SolarSystem/Moon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarSystem
+{
+    public class Moon : SpaceObject, ICalculable
+    {
+        private Planet primaryPlanet;
+
+        public Moon(Planet primaryPlanet)
+        {
+            this.primaryPlanet = primaryPlanet;
+        }
+
+        public void CalculateCoordinates(int timeLapse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SolarSystem/SolarSystem/Planet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarSystem
+{
+    public class Planet : SpaceObject, ICalculable
+    {
+        public void CalculateCoordinates(int timeLapse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
