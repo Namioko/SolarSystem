5a58a60b8accc4033471361c04a04dd1e8de8ef4
thequeen14 <flamina96@yandex.ru>
2016-11-13 17:49:29 +0300

    Added rendering of trajectory

--- SolarSystem/Visualization/MainWindow.xaml.cs
+++ SolarSystem/Visualization/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ namespace Visualization
         private Planet earth;
         private Ellipse earthEllipse;
         private SolidColorBrush earthBrush;
+        private SolidColorBrush trajectoryBrush = new SolidColorBrush(Color.FromRgb(255,255,255));
 
         public MainWindow()
         {
@@ -49,16 +50,16 @@ namespace Visualization
             earthEllipse.Fill = earthBrush;
 
             earth.ChangePosition(0);
-            Canvas.SetLeft(earthEllipse, earth.Coordinates.X);
-            Canvas.SetBottom(earthEllipse, earth.Coordinates.Y);
+            Canvas.SetLeft(earthEllipse, earth.Coordinates.X - earth.Radius);
+            Canvas.SetBottom(earthEllipse, earth.Coordinates.Y - earth.Radius);
             #endregion
 
             system.AddBody(earth);
+
             InitializeComponent();
 
             spaceCanvas.Children.Add(sunEllipse);
             spaceCanvas.Children.Add(earthEllipse);
-
         }
 
         private bool rendering = false;
@@ -76,25 +77,35 @@ namespace Visualization
 
         private void stopButton_Click(object sender, RoutedEventArgs e)
         {
+            //spaceCanvas.Children.RemoveRange(3, spaceCanvas.Children.Count - 3);
             CompositionTarget.Rendering -= RenderFrame;
             rendering = false;
         }
 
         private void RenderFrame(object sender, EventArgs e)
         {
-                foreach (var o in system)
-                {
-                    Ellipse newEllipsePoint = new Ellipse();
-                    newEllipsePoint.Fill = earthBrush;
-                    newEllipsePoint.Width = earthEllipse.Width;
-                    newEllipsePoint.Height = earthEllipse.Height;
+            foreach (var o in system)
+            {
+                Ellipse newEllipsePoint = new Ellipse();
+                newEllipsePoint.Fill = earthBrush;
+                newEllipsePoint.Width = earthEllipse.Width;
+                newEllipsePoint.Height = earthEllipse.Height;
 
-                    Canvas.SetLeft(newEllipsePoint, o.Coordinates.X);
-                    Canvas.SetBottom(newEllipsePoint, o.Coordinates.Y);
+                Canvas.SetLeft(newEllipsePoint, o.Coordinates.X - newEllipsePoint.Width/2);
+                Canvas.SetBottom(newEllipsePoint, o.Coordinates.Y - newEllipsePoint.Width/2);
 
-                    spaceCanvas.Children.Add(newEllipsePoint);
-                    spaceCanvas.Children.RemoveAt(spaceCanvas.Children.Count - 2);
-                }
+                Ellipse newTrajectoryPoint = new Ellipse();
+                newTrajectoryPoint.Fill = trajectoryBrush;
+                newTrajectoryPoint.Width = earthEllipse.Width / 10;
+                newTrajectoryPoint.Height = earthEllipse.Height / 10;
+
+                Canvas.SetLeft(newTrajectoryPoint, o.Coordinates.X - newTrajectoryPoint.Width/2);
+                Canvas.SetBottom(newTrajectoryPoint, o.Coordinates.Y - newTrajectoryPoint.Width/2);
+
+                spaceCanvas.Children.Add(newTrajectoryPoint);
+                spaceCanvas.Children.Add(newEllipsePoint);
+                spaceCanvas.Children.RemoveAt(spaceCanvas.Children.Count - 3);
+            }
         }
     }
 }
