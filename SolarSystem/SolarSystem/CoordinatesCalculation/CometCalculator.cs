51b940f59ff633926155fabe65e6d7f0d724fcc1
thequeen14 <flamina96@yandex.ru>
2016-11-24 02:34:15 +0300

    Final changes

---
+++ SolarSystem/SolarSystem/CoordinatesCalculation/CometCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using SolarSystem.ObjectsInSpace;
+
+namespace SolarSystem.CoordinatesCalculation
+{
+    public class CometCalculator : ICalculator
+    {
+        public Point CalculateCoordinates(double time, double monthsPerOneTurn, Orbit orbit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
